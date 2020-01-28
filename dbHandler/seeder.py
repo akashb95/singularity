f2c417cc4a793df940dbd7d84ff88f2bba9429a0
Akash B <akash.bhattacharya.18@ucl.ac.uk>
2020-01-28 08:01:05 +0000

    #1 File to contain functions that will seed a new database. Will be extended as more DB Entities are added.

---
+++ dbHandler/seeder.py
@@ -0,0 +1,28 @@
+from dbHandler import Base, engine, Session
+
+
+def seed_elements(num_elements: int = 150):
+    from dbHandler import Element
+    from random import uniform, choice
+
+    session = Session()
+
+    # create elements
+    status_choices = [0, 1, 2, 3, 4, 5, 15]
+    for i in range(num_elements):
+        description = "Seeded Element no. {}".format(i)
+        latitude = uniform(0, 180)
+        longitude = uniform(0, 180)
+        status = choice(status_choices)
+        element = Element(description, latitude, longitude, status)
+        session.add(element)
+
+    # commit and close
+    session.commit()
+    session.close()
+    return
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
+    seed_elements()
