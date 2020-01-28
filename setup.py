5a9856a45b8074a569aa1ec8f6860ca6d3d4a81e
Akash B <akash.bhattacharya.18@ucl.ac.uk>
2020-01-28 08:01:30 +0000

    #1 setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='stardust',
+    name='singularity',
     version='0.0.1',
-    packages=['lightingComponents'],
+    packages=['lighting', 'dbHandler', 'log'],
     url='https://github.com/akashb95/singularity',
     license='',
     author='Akash B',
@@ -11,6 +11,7 @@ setup(
     description='Fun & Games with gRPC.',
     install_requires=[
         "grpcio",
-        "grpcio-tools", 'python-dotenv'
+        "grpcio-tools", "python-dotenv",
+        "sqlalchemy", "pymysql", "cryptography"
     ]
 )
