bf209f3fe850a23639ae7cc678ff1ae54df0a1fc
Akash B <akash.bhattacharya.18@ucl.ac.uk>
2020-01-28 08:00:02 +0000

    #1 Base file for DB configuration and connection.

---
+++ dbHandler/base.py
@@ -0,0 +1,24 @@
+import os
+import sqlalchemy as db
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import logging
+from log import setup_logger
+
+import settings
+settings.load_env_vars()
+logger = setup_logger("server", logging.DEBUG)
+
+logger.debug("Creating DB Engine, DB Session and Base class.")
+
+engine = db.create_engine("{db_conn}://{user}:{password}@{host}/{db_name}"
+                          .format(db_conn=os.getenv("DB_CONN"),
+                                  user=os.getenv("DB_USER"),
+                                  password=os.getenv("DB_PASS"),
+                                  host=os.getenv("DB_HOST"),
+                                  db_name=os.getenv("DB_NAME")))
+
+logger.debug("Connected to {}".format(engine))
+
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
