9cb057f379a361254482a70c50a17dcd0641dcde
Akash B <akash.bhattacharya.18@ucl.ac.uk>
2020-01-30 13:41:53 +0000

    #4 Corrected import order - fixes circular imports

--- dbHandler/__init__.py
+++ dbHandler/__init__.py
@@ -1,5 +1,6 @@
+# base MUST be imported first
 from .asset import Asset
-from .base import Base, Session, engine
+from .base import Base, engine
 from .basestation import Basestation
 from .element import Element
 from .seeder import seed
