194999a75153b9ff3ed9c4ab10042983a233ea82
Akash B <akash.bhattacharya.18@ucl.ac.uk>
2020-01-30 14:01:19 +0000

    #4

--- dbHandler/__init__.py
+++ dbHandler/__init__.py
@@ -1,7 +1,10 @@
 # base MUST be imported first
-from .asset import Asset
 from .base import Base, engine
+
+from .asset import Asset
 from .basestation import Basestation
 from .element import Element
-from .seeder import seed
 from .telecell import Telecell
+from .user import User
+
+from .seeder import seed_lighting_components
