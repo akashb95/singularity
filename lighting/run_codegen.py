bf7e3a942e33725235a2f669d3624b9f0da1bbf3
Akash B <akash.bhattacharya.18@ucl.ac.uk>
2020-02-01 13:36:18 +0000

    #14 Automatically fix import paths in generated Python files Also tell protoc to generate files in a separate subdirectory.

--- lighting/run_codegen.py
+++ lighting/run_codegen.py
@@ -1,17 +1,46 @@
 """Runs protoc with the gRPC plugin to generate messages and gRPC stubs."""
+import logging
+from log import setup_logger
 
 from grpc_tools import protoc
+import os
+import re
 
-protoc.main((
-    '',
-    '-I../protos',
-    '--python_out=.',
-    '--grpc_python_out=.',
-    '../protos/asset.proto',
-    '../protos/basestation.proto',
-    '../protos/element.proto',
-    '../protos/location.proto',
-    '../protos/telecell.proto',
-    '../protos/ts.proto',
-    '../protos/user.proto'
-))
+generated_files_outdir = "./lib"
+
+logger = setup_logger("codegen", logging.DEBUG)
+
+if __name__ == "__main__":
+    protoc.main((
+        '',
+        '-I../protos',
+        '--python_out={}'.format(generated_files_outdir),
+        '--grpc_python_out={}'.format(generated_files_outdir),
+        '../protos/asset.proto',
+        '../protos/basestation.proto',
+        '../protos/element.proto',
+        '../protos/location.proto',
+        '../protos/telecell.proto',
+        '../protos/ts.proto',
+        '../protos/user.proto'
+    ))
+
+    abs_path_codegen_outdir = os.path.join(os.getcwd(), generated_files_outdir)
+
+    for file in os.listdir(abs_path_codegen_outdir):
+        # look for only .py files - skip all others
+        if not file.endswith(".py"):
+            continue
+
+        logger.info("Modifying imports in file: {}".format(file))
+
+        # read in code to memory
+        with open(os.path.join(abs_path_codegen_outdir, file), "r") as generated_file:
+            code = generated_file.read()
+
+        # modify code in memory using regex
+        code_modified_imports = re.sub("(import .*_pb2 as .*__pb2)", r"from . \1", code, flags=re.M)
+
+        # write to code file
+        with open(os.path.join(abs_path_codegen_outdir, file), "w") as generated_file:
+            generated_file.write(code_modified_imports)
