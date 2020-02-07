34aa61aeb5a5e9e6530af740b08998cd2148f5c0
Akash B <akash.bhattacharya.18@ucl.ac.uk>
2020-02-07 07:33:21 +0000

    #9 Added client-side code to test new RPCs defined in lighting.server.handler.ElementHandler

--- lighting/client/client.py
+++ lighting/client/client.py
@@ -1,13 +1,13 @@
 import logging
-from log import setup_logger
 import os
-import settings as lighting_settings
 
 import grpc
 
 import lighting.lib.element_pb2 as element_pb2
 import lighting.lib.location_pb2 as location_pb2
+import settings as lighting_settings
 from lighting.lib.element_pb2_grpc import ElementStub
+from log import setup_logger
 
 logger = setup_logger("client", logging.DEBUG)
 
@@ -26,7 +26,7 @@ def run(port: int):
     stub = ElementStub(channel)
 
     # search by id
-    response = stub.Get(element_pb2.Request(id=9))
+    response = stub.Get(element_pb2.Request(id=9000))
     logger.info("Element {} ({}, {}) connected to Asset {}, status: {} \n{}"
                 .format(response.id, response.location.lat, response.location.long, response.asset.id,
                         element_pb2.ActivityStatus.Name(response.status),
@@ -34,7 +34,7 @@ def run(port: int):
 
     # search by location
     map_rectangle = location_pb2.MapRect(lo=location_pb2.Location(long=0, lat=0),
-                                         hi=location_pb2.Location(long=100, lat=10))
+                                         hi=location_pb2.Location(long=100, lat=100))
     search_response = stub.SearchByLocation(location_pb2.FilterByLocationRequest(rectangle=map_rectangle))
 
     # need to iterate over all returned replies before channel is closed
@@ -52,6 +52,53 @@ def run(port: int):
                         .format(i + 1, resp.id, resp.location.lat, resp.location.long, resp.asset.id,
                                 element_pb2.ActivityStatus.Name(resp.status),
                                 resp.description))
+
+    # Create Elements
+    message = element_pb2.CreateRequest()
+    elements = []
+    for i in range(4):
+        elements.append(element_pb2.Reply(description="{} Created by client".format(i), no_location=True))
+    message.elements.extend(elements)
+
+    response_all = stub.Create(message)
+    for i, resp in enumerate(response_all.elements):
+        logger.info("{} Element {} ({}, {}) connected to Asset {}, status: {} \n{}"
+                    .format(i + 1, resp.id, resp.location.lat, resp.location.long, resp.asset.id,
+                            element_pb2.ActivityStatus.Name(resp.status),
+                            resp.description))
+
+    # Update Element
+    message = element_pb2.Reply(id=84, status=1, description="Client Updated Element")
+    message.location.long, message.location.lat = 5, 5
+    resp = stub.Update(message)
+    logger.info("Element {} ({}, {}) connected to Asset {}, status: {} \n{}"
+                .format(resp.id, resp.location.lat, resp.location.long, resp.asset.id,
+                        element_pb2.ActivityStatus.Name(resp.status),
+                        resp.description))
+
+    # Soft-Delete Element
+    message = element_pb2.Request(id=84)
+    resp = stub.Delete(message)
+    logger.info("Element {} ({}, {}) connected to Asset {}, status: {} \n{}"
+                .format(resp.id, resp.location.lat, resp.location.long, resp.asset.id,
+                        element_pb2.ActivityStatus.Name(resp.status),
+                        resp.description))
+
+    # Prune (permanently delete) Element
+    message = element_pb2.Request(id=84)
+    resp = stub.Prune(message)
+    logger.info("Element {} ({}, {}) connected to Asset {}, status: {} \n{}"
+                .format(resp.id, resp.location.lat, resp.location.long, resp.asset.id,
+                        element_pb2.ActivityStatus.Name(resp.status),
+                        resp.description))
+
+    # Add Element to Asset
+    message = element_pb2.Reply(id=489, asset_id=4)
+    resp = stub.AddToAsset(message)
+    logger.info("Element {} ({}, {}) connected to Asset {}, status: {} \n{}"
+                .format(resp.id, resp.location.lat, resp.location.long, resp.asset.id,
+                        element_pb2.ActivityStatus.Name(resp.status),
+                        resp.description))
 
     # close channel.
     channel.close()
