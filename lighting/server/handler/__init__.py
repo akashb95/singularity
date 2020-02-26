8670474117c665443f5ee0df993a2c101879847c
Akash B <akash.bhattacharya.18@ucl.ac.uk>
2020-02-26 21:41:09 +0000

    #21 Remove lighting microservice from this branch.
    This branch will henceforth only contain stuff relating to:
    - proto definitions
    - env file template
    - common README
    
    i.e. files common to the entire repo across all branches.

--- lighting/server/handler/__init__.py
+++
@@ -1,5 +0,0 @@
-from .asset import AssetHandler
-from .basestation import BasestationHandler
-from .element import ElementHandler
-from .telecell import TelecellHandler
-from .user import UserHandler
--- settings.py
+++
@@ -1,5 +0,0 @@
-from dotenv import load_dotenv, find_dotenv
-
-def load_env_vars(debug=True):
-    load_dotenv(find_dotenv(), verbose=debug)
-    return
